be8f2703de921e4c12f2828c36b9ac59481a4db5
asharif02 <asharif02@gmail.com>
2022-04-18 15:08:04 -0400

    Able to like and dislike items shown to me

--- src/main/java/com/amazon/AmazonHomePage.java
+++ src/main/java/com/amazon/AmazonHomePage.java
@@ -28,10 +28,47 @@ public class AmazonHomePage extends BasePage {
     @FindBy(xpath = "//a[@href='/stylequiz?ref=sq_sbs_mn']")
     public WebElement homeStyleQuizButton;
 
+    @FindBy(xpath = "//a[@href='/discover/?_encoding=UTF8&ref_=sv_hg_2']")
+    public WebElement discoverHyperlinkHover;
+
+    @FindBy(xpath = "//a[@href='/discover/?ref=sbl_mm_lp']")
+    public WebElement discoverPanel;
+
+    @FindBy(xpath = "//a[@class='landing-header-welcome-header-focus mirai-clickable']")
+    public WebElement startLikingNowButton;
+
+    @FindBy(xpath = "//div[@data-asin='B09QKQWJGW']//img[@alt='dislike']")
+    public WebElement dislikeButtonOne;
+
+    @FindBy(xpath = "//div[@data-asin='B07N8R9WKN']//img[@alt='like']")
+    public WebElement likeButtonOne;
+
+    @FindBy(xpath = "//div[@data-asin='B09KLM2J3B']//img[@alt='like']")
+    public WebElement likeButtonTwo;
+
+    @FindBy(xpath = "//div[@data-asin='B07DLTBWM3']//img[@alt='like']")
+    public WebElement likeButtonThree;
+
+    @FindBy(xpath = "//div[@data-asin='B089GY587K']//img[@alt='dislike']")
+    public WebElement dislikeButtonTwo;
+
+    @FindBy(xpath = "//div[@data-asin='B07CJZQ6N4']//img[@alt='dislike']")
+    public WebElement dislikeButtonThree;
+
+    @FindBy(xpath = "//div[@data-asin='B08V8Y5HL7']//img[@alt='like']")
+    public WebElement likeButtonFour;
+
+    @FindBy(xpath = "//div[@data-asin='B08YS4WSBY']//img[@alt='like']")
+    public WebElement likeButtonFive;
+
+    @FindBy(xpath = "//div[@data-asin='B009OBA2V8']//img[@alt='like']")
+    public WebElement likeButtonSix;
+
+
+
     public AmazonHomePage() {
         webDriverWait.until(ExpectedConditions.urlContains(URL));
         PageFactory.initElements(driver, this);
     }
-
 
 }
--- src/test/java/com/amazon/TestAmazonHomePage.java
+++ src/test/java/com/amazon/TestAmazonHomePage.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 
 public class TestAmazonHomePage extends TestBasePage {
 
+    // region 1st test case
     @Test
     public void testFurnitureSection() {
         Homepage homepage = new Homepage();
@@ -22,20 +23,40 @@ public class TestAmazonHomePage extends TestBasePage {
         Assert.assertEquals(actualTitle, expectedTitle, "ACTUAL DOES NOT MATCH EXPECTED RESULT");
 
     }
+    // endregion
 
+    // 2nd test case TBD
+
+    // 3rd test case
     @Test
     public void testHoverOnAmazonHomeSubnav() {
         Homepage homepage = new Homepage();
         AmazonHomePage amazonHomePage = homepage.navigateToAmazonHomePage();
         Actions actions = new Actions(driver);
 
-        actions.moveToElement(amazonHomePage.shopByStyleHover);
-        actions.moveToElement(amazonHomePage.homeStyleQuizButton);
+        // hovering on Discover Hyperlink
+        actions.moveToElement(amazonHomePage.discoverHyperlinkHover);
         actions.click().build().perform();
+
+        amazonHomePage.startLikingNowButton.click();
+
+        amazonHomePage.dislikeButtonOne.click();
+        amazonHomePage.likeButtonOne.click();
+        amazonHomePage.likeButtonTwo.click();
+        amazonHomePage.likeButtonThree.click();
+        amazonHomePage.dislikeButtonTwo.click();
+        amazonHomePage.dislikeButtonThree.click();
+        amazonHomePage.likeButtonFour.click();
+        amazonHomePage.likeButtonFive.click();
+        amazonHomePage.likeButtonSix.click();
 
 
     }
 
+
+    // 4th test case TBD
+
+    // 5th test case TBD
 
 
 }
