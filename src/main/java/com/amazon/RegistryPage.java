39fd1fc8f80f441e543221ae78ea2c407b28d9ac
RomanGad <romanergogo@gmail.com>
2022-04-19 13:09:40 -0400

    complted birthday gift list button test

--- src/main/java/com/amazon/RegistryPage.java
+++ src/main/java/com/amazon/RegistryPage.java
@@ -1,6 +1,20 @@
 package com.amazon;
 
 import base.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class RegistryPage extends BasePage {
+    private static final String URL = "https://www.amazon.com";
+
+    @FindBy(xpath = "(//img[@class='gr-card__img'])[3]")
+    public WebElement birthdayGiftListButton;
+
+    public RegistryPage(){
+        webDriverWait.until(ExpectedConditions.urlContains(URL));
+        PageFactory.initElements(driver, this);
+
+    }
 }
--- src/test/java/com/amazon/TestRegistryPage.java
+++ src/test/java/com/amazon/TestRegistryPage.java
@@ -1,6 +1,14 @@
 package com.amazon;
 
 import base.TestBasePage;
+import org.testng.annotations.Test;
 
 public class TestRegistryPage extends TestBasePage {
+
+    @Test
+    public void testBirthdayGiftListButton(){
+        Homepage homepage=new Homepage();
+        RegistryPage registryPage= homepage.navigateToRegistryPage();
+        registryPage.birthdayGiftListButton.click();
+    }
 }
