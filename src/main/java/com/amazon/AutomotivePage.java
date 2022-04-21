bf3ef752cc0df29381fa63f22a3a369dbc212419
maliksaabb <usmandotmalik@gmail.com>
2022-04-21 17:30:50 -0400

    pushing 2nd and 3rd test cases

--- src/main/java/com/amazon/AutomotivePage.java
+++ src/main/java/com/amazon/AutomotivePage.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.net.URL;
 
+//testcase1
 public class AutomotivePage extends BasePage {
     private static final String URL = "https://www.amazon.com";
 
@@ -19,6 +20,32 @@ public class AutomotivePage extends BasePage {
 
     @FindBy(xpath = "//*[@id='nav-search-submit-button']")
     public WebElement searchSubmitButton;
+
+    //testCase2
+    @FindBy(xpath = "//option[@value='search-alias=automotive']")
+    public WebElement automotiveScrollOption2;
+
+    @FindBy(xpath = "//*[@id='nav-search-submit-button']")
+    public WebElement searchSubmitButton2;
+
+    @FindBy(xpath = "//a[@title='Oils & Fluids']")
+    public WebElement oilnFluidsButton;
+
+    //testCase3
+    @FindBy(xpath = "//option[@value='search-alias=automotive']")
+    public WebElement automotiveScrollOption3;
+
+    @FindBy(xpath = "//*[@id='nav-search-submit-button']")
+    public WebElement searchSubmitButton3;
+
+    @FindBy(xpath = "//a[@title='Oils & Fluids']")
+    public WebElement oilnFluidsButton3;
+
+    @FindBy(xpath = "//*[@class='a-size-base-plus a-color-base a-text-normal']")
+    public  WebElement oilnFluidPumpItem;
+
+
+
     public AutomotivePage(){
         webDriverWait.until(ExpectedConditions.urlContains(URL));
         PageFactory.initElements(driver,this);
--- src/test/java/com/amazon/TestAutomotivePage.java
+++ src/test/java/com/amazon/TestAutomotivePage.java
@@ -13,4 +13,23 @@ public class TestAutomotivePage extends TestBasePage {
         automotivePage.searchSubmitButton.click();
         automotivePage.carCareButton.click();
     }
+
+    @Test
+    public void testoilnFluidsButton(){
+        Homepage homepage = new Homepage();
+        AutomotivePage automotivePage = homepage.navigateToAutomotivePage();
+        automotivePage.automotiveScrollOption2.click();
+        automotivePage.searchSubmitButton2.click();
+        automotivePage.oilnFluidsButton.click();
+    }
+
+    @Test
+    public void testoilFluidPumpItem() {
+        Homepage homepage = new Homepage();
+        AutomotivePage automotivePage = homepage.navigateToAutomotivePage();
+        automotivePage.automotiveScrollOption3.click();
+        automotivePage.searchSubmitButton3.click();
+        automotivePage.oilnFluidsButton3.click();
+        automotivePage.oilnFluidPumpItem.click();
+    }
 }
