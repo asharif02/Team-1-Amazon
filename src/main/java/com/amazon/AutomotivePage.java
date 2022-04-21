2a0818bf74d770f8517032f17c06e738fcad0271
maliksaabb <usmandotmalik@gmail.com>
2022-04-21 17:56:02 -0400

    Commenting on testCases

--- src/main/java/com/amazon/AutomotivePage.java
+++ src/main/java/com/amazon/AutomotivePage.java
@@ -8,10 +8,11 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.net.URL;
 
-//testcase1
+
 public class AutomotivePage extends BasePage {
     private static final String URL = "https://www.amazon.com";
 
+    //TestCase1- Navigating to automotive button and testing the Car Care button functionality
     @FindBy(xpath = "//a[@title='Car Care']")
     public WebElement carCareButton;
 
@@ -21,7 +22,7 @@ public class AutomotivePage extends BasePage {
     @FindBy(xpath = "//*[@id='nav-search-submit-button']")
     public WebElement searchSubmitButton;
 
-    //testCase2
+    //testCase2-  Testing the Oil & Fluids Button functionality
     @FindBy(xpath = "//option[@value='search-alias=automotive']")
     public WebElement automotiveScrollOption2;
 
@@ -31,7 +32,7 @@ public class AutomotivePage extends BasePage {
     @FindBy(xpath = "//a[@title='Oils & Fluids']")
     public WebElement oilnFluidsButton;
 
-    //testCase3
+    //testCase3- Functionality of Fluid pump item button in Oil & Fluids category
     @FindBy(xpath = "//option[@value='search-alias=automotive']")
     public WebElement automotiveScrollOption3;
 
@@ -44,7 +45,7 @@ public class AutomotivePage extends BasePage {
     @FindBy(xpath = "//*[@class='a-size-base-plus a-color-base a-text-normal']")
     public  WebElement oilnFluidPumpItem;
 
-    //testCase4
+    //testCase4- testing the functionality of fluidPump video button
     @FindBy(xpath = "//option[@value='search-alias=automotive']")
     public WebElement automotiveScrollOption4;
 
@@ -59,6 +60,9 @@ public class AutomotivePage extends BasePage {
 
     @FindBy(xpath = "//*[@id='a-autoid-15']")
     public WebElement fluidPumpVideo;
+
+    //testcase5
+
 
 
 
