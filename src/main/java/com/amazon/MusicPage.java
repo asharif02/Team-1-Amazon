e4a32795e8ac9ac66bb8e2b5bbe32824a43ca722
LucN1 <lucnguyen.livelife@gmail.com>
2022-04-21 20:23:32 -0400

    Lalala MusicTab

--- src/main/java/com/amazon/MusicPage.java
+++ src/main/java/com/amazon/MusicPage.java
@@ -1,6 +1,16 @@
 package com.amazon;
 
 import base.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class MusicPage extends BasePage {
+
+    // Set and start at amazon url
+    public static final String url = "https://www.amazon.com";
+
+    // Test Case 1, Navigate to Music page
+    @FindBy(xpath = "//div[@class='nav-a ']")
+    public WebElement MusicTab;
+
 }
--- src/test/java/com/amazon/TestMusicPage.java
+++ src/test/java/com/amazon/TestMusicPage.java
@@ -1,6 +1,15 @@
 package com.amazon;
 
 import base.TestBasePage;
+import org.testng.annotations.Test;
 
 public class TestMusicPage extends TestBasePage {
+
+    // Test Case 1
+    @Test
+    public void testMusicTab() {
+        Homepage homepage = new Homepage();
+        MusicPage musicPage = homepage.navigateToMusicPage();
+        musicPage.MusicTab.click();
+    }
 }
