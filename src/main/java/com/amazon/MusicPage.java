574128dffb6715770d90bd1852eb74fe3e6eb199
LucN1 <lucnguyen.livelife@gmail.com>
2022-04-22 16:36:11 -0400

    6 test cases completed, with notes, double check later

--- src/main/java/com/amazon/MusicPage.java
+++ src/main/java/com/amazon/MusicPage.java
@@ -12,20 +12,41 @@ public class MusicPage extends BasePage {
     // Set and start at amazon url
     public static final String URl = "https://www.amazon.com";
 
-    // Test Case 1, Navigate to Music page
-    @FindBy(xpath = "//*[@id=\"nav-xshop\"]/a[8]")
+    // Copying Xpath is a short way, but it doesn't work for everything
+    // Better to learn how to manually write them out
+
+    // TestCase1, Navigate to Music page
+    @FindBy(xpath = "//a[@href='/music/unlimited?ref_=nav_cs_music']")
     public WebElement MusicTab;
 
-    // Test Case 2, Try it free button, its a two step process, fixed your xpath !
-    @FindBy(xpath = "//*[@id=\"nav-xshop\"]/a[8]")
-    public WebElement MusicTab2;
+    // TestCase2, Try it free button, two steps process.
     @FindBy(xpath = "//span[@id='a-autoid-0-announce']")
     public WebElement TryItFreeButton;
 
 
-    // Test Case 3, Open web player
-    @FindBy(xpath = "//*[@id=\"nav-subnav\"]/a[9]")
+    // TestCase3, Open web player
+    @FindBy(xpath = "//a[@href='https://music.amazon.com/?_encoding=UTF8&ref_=sv_dmusic_8']")
     public WebElement OpenWebPlayer;
+
+    // TestCase4, see more The HD Difference
+    @FindBy(xpath = "//a[@aria-label='hd and ultra hd']")
+    public WebElement SeeMoreHDDifference;
+
+    //TestCase5, Amazon music prime
+    //@FindBy(xpath = "//*[@id=\"nav-subnav\"]/a[3]/span[1]")
+    @FindBy(xpath = "//a[@data-nav-key='aj:https://m.media-amazon.com/images/G/01/MUSIC/CCMP/LandingPages/subnav/subnav_us_en_1.json:subnav-dmusic-flyout-prime:0']")
+    public WebElement AmazonMusicPrime;
+
+    // TestCase6, Sign in with Email & Password values (Don't have to, but include wait time)
+    @FindBy(xpath = "//input[@id='ap_email']")
+    public WebElement SignInEmail;
+    @FindBy(xpath = "//input[@id='ap_password']")
+    public WebElement SignInPassword;
+    @FindBy(xpath = "//input[@id='signInSubmit']")
+    public WebElement SignInSubmit;
+
+
+
 
     public MusicPage () {
         webDriverWait.until(ExpectedConditions.urlContains(URl));
--- src/test/java/com/amazon/TestMusicPage.java
+++ src/test/java/com/amazon/TestMusicPage.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 public class TestMusicPage extends TestBasePage {
 
-    // Test Case 1, Navigate to Music page
+    // TestCase1, Navigate to Music page
     @Test
     public void testMusicTab() {
         Homepage homepage = new Homepage();
@@ -13,20 +13,52 @@ public class TestMusicPage extends TestBasePage {
         musicPage.MusicTab.click();
     }
 
-    // Test Case 2, Try it free button
+    // TestCase2, Try it free button
     @Test
     public void testTryItFreeButton() {
         Homepage homepage = new Homepage();
         MusicPage musicPage = homepage.navigateToMusicPage();
-        musicPage.MusicTab2.click();
         musicPage.TryItFreeButton.click();
         }
 
-    // Test Case 3, Open web player
+    // TestCase3, Open web player
     @Test
     public void testOpenWebPlayer() {
         Homepage homepage = new Homepage();
-        MusicPage openWebPlayer = homepage.navigateToMusicPage();
-        openWebPlayer.OpenWebPlayer.click();
+        MusicPage musicPage = homepage.navigateToMusicPage();
+        musicPage.OpenWebPlayer.click();
+    }
+
+    // TestCase4, see more the HD difference
+    @Test
+    public void testSeeMoreHDDifference() {
+        Homepage homepage = new Homepage();
+        MusicPage musicPage = homepage.navigateToMusicPage();
+        musicPage.SeeMoreHDDifference.click();
+    }
+
+    // TestCase5, Amazon music prime
+    @Test
+    public void testAmazonMusicPrime() {
+        Homepage homepage = new Homepage();
+        MusicPage musicPage = homepage.navigateToMusicPage();
+        musicPage.AmazonMusicPrime.click();
+    }
+
+    // TestCase6, Sign in with Email & Password values
+    // you can add throws InterruptedException in the method, therefore wait can work
+    // Thread.sleep(1000)=1 second under a code, therefore it will give it a wait
+    // Just in case it was runs too fast
+    @Test
+    public void testSignInEmail() throws InterruptedException {
+        Homepage homepage = new Homepage();
+        MusicPage musicPage = homepage.navigateToMusicPage();
+        musicPage.TryItFreeButton.click();
+        Thread.sleep(1000);
+        musicPage.SignInEmail.click();
+        musicPage.SignInEmail.sendKeys("KylieJenner@gmail.com");
+        musicPage.SignInPassword.click();
+        musicPage.SignInPassword.sendKeys("Baby123$");
+        musicPage.SignInSubmit.click();
     }
 }
